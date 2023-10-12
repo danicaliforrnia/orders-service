ca2d8066f020533e45b427ae81b0a6cb08ee5b40
stefanellid <stefanellid@media-saturn.com>
2023-10-12 14:29:03 +0200

    feat: add log

--- src/main/java/com/imagina/ordersservice/controllers/OrdersController.java
+++ src/main/java/com/imagina/ordersservice/controllers/OrdersController.java
@@ -4,13 +4,19 @@ import com.imagina.ordersservice.dto.InputOrderDto;
 import com.imagina.ordersservice.dto.ReadOrderDto;
 import com.imagina.ordersservice.services.OrdersService;
 import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("orders")
 public class OrdersController {
+
+    @Value("${eureka.instance.instance-id}")
+    private String instanceId;
 
     private final OrdersService ordersService;
 
@@ -26,6 +32,7 @@ public class OrdersController {
 
     @PostMapping(consumes = "application/json", produces = "application/json")
     public ReadOrderDto create(@Valid @RequestBody InputOrderDto inputOrderDto) {
+        log.info("Instance ID: {}", instanceId);
         return ordersService.create(inputOrderDto);
     }
 }
